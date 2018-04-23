5ae3c120123645d60b543e16104cc7a01f9c388f
Richardson, Mark <BusinessGuy1979@gmail.com>
2018-04-23 15:41:11 -0500

    Checking in.

---
+++ 4. Services/.Net Core/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdyGuy.Services.NetCore.Example.Middleware
+{
+    public class LoggingMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            nextDelegate = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Debug.WriteLine("=== Logging Before ===");
+            await nextDelegate.Invoke(context);
+            Debug.WriteLine("=== Logging After ===");
+        }
+    }
+}
--- 4. Services/.Net Core/Startup.cs
+++ 4. Services/.Net Core/Startup.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using NerdyGuy.Services.NetCore.Example.Middleware;
 
 namespace NerdyGuy.Services.NetCore.Example
 {
@@ -29,12 +32,9 @@ namespace NerdyGuy.Services.NetCore.Example
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseMiddleware<LoggingMiddleware>();
 
-            app.UseMvc();
+            app.UseStaticFiles();
         }
     }
 }
