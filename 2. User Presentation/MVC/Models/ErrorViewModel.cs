34b84a3726c2542c6f58b2e8df3bc7e9445776fe
Richardson, Mark <BusinessGuy1979@gmail.com>
2018-04-26 08:44:17 -0500

    Checking in working version of Identity server.

---
+++ 2. User Presentation/MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NerdyGuy.UserPresentation.MVC.Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
